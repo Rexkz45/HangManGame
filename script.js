9c2c76704205e4b617d8674890ad7ac2854f403c
Rexkz45 <rexkz45@gmail.com>
2026-04-02 23:11:36 -0700

    added some part of js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+// creating buttons using js 
 const keyboard=document.querySelector(".keyboard");
+const hintDisplay=document.querySelector(".hintDisplay b")
+const randomWord=()=>{
+const {hint,word}=wordList[Math.floor(Math.random()*wordList.length)];
+// console.log(hint);
+console.log(word);
+hintDisplay.innerHTML=hint;
+
+}
+// Map
+// split 
+// join
+
+
 for(let i=97;i<=122;i++){
+    // console.log(String.fromCharCode(i));
     const button=document.createElement("button");
     button.innerHTML=String.fromCharCode(i);
+    // console.log(button)
     keyboard.appendChild(button);
 }
+
+
+randomWord();
