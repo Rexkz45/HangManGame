70489d720476dd88f1754aa8264765cadd78ed9a
Rexkz45 <rexkz45@gmail.com>
2026-04-05 23:35:23 -0700

    init gaem done

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-// creating buttons using js 
+// variables
 const keyboard=document.querySelector(".keyboard");
-const hintDisplay=document.querySelector(".hintDisplay b")
+const hintDisplay=document.querySelector(".hintDisplay b");
+const wordDisplay=document.querySelector(".wordDisplay");
+let currentWord;
 const randomWord=()=>{
-const {hint,word}=wordList[Math.floor(Math.random()*wordList.length)];
-// console.log(hint);
+const {word ,hint}=wordList[Math.floor(Math.random()*50)];
+currentWord=word;
 console.log(word);
 hintDisplay.innerHTML=hint;
+wordDisplay.innerHTML=word.split("").map(()=>
+  '<li class="letter"></li>'
+).join("");
+// console.log(word.split(""));
+//  css c ,s,s
 
 }
-// Map
-// split 
-// join
-
+const initGame=(button,pressedLetter)=>{
+// console.log(button);
+// console.log(pressedLetter);
+if(currentWord.includes(pressedLetter)){
+  console.log("word hai");
+}
+else{
+  console.log("word nhi match hua ");
+}
+}
 
 for(let i=97;i<=122;i++){
-    // console.log(String.fromCharCode(i));
-    const button=document.createElement("button");
-    button.innerHTML=String.fromCharCode(i);
-    // console.log(button)
-    keyboard.appendChild(button);
+const button=document.createElement("button");
+button.innerHTML=String.fromCharCode(i);
+keyboard.appendChild(button);
+button.addEventListener("click",e => initGame(e.target,String.fromCharCode(i)))
 }
-
 
 randomWord();
