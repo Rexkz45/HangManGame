e61612297c136f47ed8b7ca4cdd2055a7272c5e5
Rexkz45 <rexkz45@gmail.com>
2026-04-06 23:16:42 -0700

    done till image update

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-// variables
-const keyboard=document.querySelector(".keyboard");
-const hintDisplay=document.querySelector(".hintDisplay b");
-const wordDisplay=document.querySelector(".wordDisplay");
-let currentWord;
-const randomWord=()=>{
-const {word ,hint}=wordList[Math.floor(Math.random()*50)];
-currentWord=word;
-console.log(word);
-hintDisplay.innerHTML=hint;
-wordDisplay.innerHTML=word.split("").map(()=>
-  '<li class="letter"></li>'
-).join("");
-// console.log(word.split(""));
-//  css c ,s,s
+const keyboard = document.querySelector(".keyboard");
+const hintDisplay = document.querySelector(".hintDisplay b");
+const wordDisplay = document.querySelector(".wordDisplay");
+const guessDisplay=document.querySelector(".guessDisplay b");
+const hangPic=document.querySelector(".hangPic");
+let currentWord , wrongGuess=0;
+const maxGuess=6;
+const randomWord = () => {
+  const { word, hint } = wordList[Math.floor(Math.random() * 50)];
+  currentWord = word;
+  hintDisplay.innerHTML = hint;
+  console.log(word);
+  wordDisplay.innerHTML = word
+    .split("")
+    .map(() => ` <li class="letter"></li>`)
+    .join("");
+};
+const initGame = (button, pressedLetter) => {
+  // console.log(pressedLetter)
+  // console.log(button)
+  if (currentWord.includes(pressedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+      if(letter === pressedLetter){
+        wordDisplay.querySelectorAll("li")[index].innerHTML=letter;
+      }
+    });
+  } else {
+    wrongGuess++;
+    hangPic.src=`assets/hangman-${wrongGuess}.svg`;
+  }
+ guessDisplay.innerHTML=`${wrongGuess}/${maxGuess}`;
+ 
+ 
 
-}
-const initGame=(button,pressedLetter)=>{
-// console.log(button);
-// console.log(pressedLetter);
-if(currentWord.includes(pressedLetter)){
-  console.log("word hai");
-}
-else{
-  console.log("word nhi match hua ");
-}
-}
-
-for(let i=97;i<=122;i++){
-const button=document.createElement("button");
-button.innerHTML=String.fromCharCode(i);
-keyboard.appendChild(button);
-button.addEventListener("click",e => initGame(e.target,String.fromCharCode(i)))
+};
+for (let i = 97; i <= 122; i++) {
+  const button = document.createElement("button");
+  button.innerHTML = String.fromCharCode(i);
+  keyboard.appendChild(button);
+  button.addEventListener("click", (e) =>
+    initGame(e.target, String.fromCharCode(i)),
+  );
 }
 
 randomWord();
