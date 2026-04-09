a6257050044ea55718933f317d60f36ee0fd283a
Rexkz45 <rexkz45@gmail.com>
2026-04-08 23:34:52 -0700

    done till  initgame

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 const keyboard = document.querySelector(".keyboard");
 const hintDisplay = document.querySelector(".hintDisplay b");
-const wordDisplay = document.querySelector(".wordDisplay");
-const guessDisplay=document.querySelector(".guessDisplay b");
-const hangPic=document.querySelector(".hangPic");
-let currentWord , wrongGuess=0;
+const wordDisplay = document.querySelector(".wordDisplay ");
+const guessDisplay = document.querySelector(".guessDisplay  b");
+const hangPic = document.querySelector(".hangPic");
+
+let curentWord , wrongGuess=0;
 const maxGuess=6;
 const randomWord = () => {
-  const { word, hint } = wordList[Math.floor(Math.random() * 50)];
-  currentWord = word;
+  const { hint, word } = wordList[Math.floor(Math.random() * 50)];
+  curentWord = word;
   hintDisplay.innerHTML = hint;
   console.log(word);
   wordDisplay.innerHTML = word
     .split("")
-    .map(() => ` <li class="letter"></li>`)
+    .map(() => `<li class="letter"></li>`)
     .join("");
 };
 const initGame = (button, pressedLetter) => {
-  // console.log(pressedLetter)
   // console.log(button)
-  if (currentWord.includes(pressedLetter)) {
-    [...currentWord].forEach((letter, index) => {
-      if(letter === pressedLetter){
-        wordDisplay.querySelectorAll("li")[index].innerHTML=letter;
-      }
-    });
+  // console.log(pressedLetter)
+  if (curentWord.includes(pressedLetter)) {
+    [...curentWord].forEach((letter,index)=>{
+     if(letter===pressedLetter){
+      wordDisplay.querySelectorAll("li")[index].innerHTML=letter;
+      wordDisplay.querySelectorAll("li")[index].classList.add("show");
+     }
+    })
   } else {
     wrongGuess++;
-    hangPic.src=`assets/hangman-${wrongGuess}.svg`;
-  }
- guessDisplay.innerHTML=`${wrongGuess}/${maxGuess}`;
- 
- 
+     hangPic.src=`assets/hangman-${wrongGuess}.svg`; 
 
+  }
+  guessDisplay.innerHTML=`${wrongGuess}/${maxGuess}`;
+  button.disabled=true;
 };
 for (let i = 97; i <= 122; i++) {
   const button = document.createElement("button");
@@ -41,5 +42,4 @@ for (let i = 97; i <= 122; i++) {
     initGame(e.target, String.fromCharCode(i)),
   );
 }
-
 randomWord();
